4e805327255a3f16b20150ea0513ab4341e5e961
Cihan Yayla <cihan@Cihans-MacBook-Air.local>
2024-04-11 18:35:41 +0300

    initial 2
    second initial commit

--- .config/nvim/lua/cihan/plugins/copilot.lua
+++ .config/nvim/lua/cihan/plugins/copilot.lua
@@ -1,4 +1,11 @@
 return {
   "github/copilot.vim",
   event = "VeryLazy",
+  config = function()
+    vim.keymap.set("i", "<C-L>", 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+    })
+    vim.g.copilot_no_tab_map = true
+  end,
 }
